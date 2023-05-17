e8227032bb59c551c6a250280924cd274baa8072
dineshtiwari69 <210343@softwarica.edu.np>
2023-05-17 18:28:06 +0545

    Chnage Logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,8 +5,9 @@ export default function Header() {
   return (
     <header className="flex gap-3 items-center  ">
       <h1 className="text-3xl text-pink-100 font-extrabold animate-pulse flex gap-3">
-        <Diamonds className="h-8 w-8" />
-        PTC
+        {/* <Diamonds className="h-8 w-8" /> */}
+        <img src="/brandassets/logo.png" className="h-14 w-14" />
+       
       </h1>
 
       <div className="ml-auto">
--- components/Preloader/PreLoader.tsx
+++ components/Preloader/PreLoader.tsx
@@ -4,7 +4,8 @@ export default function PreLoader() {
     return (
         <div className="flex justify-center items-center h-screen">
             <div className='text-pink-100 text-center'>
-                <Diamonds className="h-[100px] w-[100px] animate-pulse" />
+                <img src='/brandassets/logo.png' className="h-[100px] w-[100px] animate-pulse" />
+                {/* <Diamonds className="h-[100px] w-[100px] animate-pulse" /> */}
                 
             </div>
 
